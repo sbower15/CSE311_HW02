bb23f9fa83e395d14852532e609ebf175fd195dd
Steven Bower <bowersj@miamioh.edu>
2018-02-26 17:06:20 -0500

    Complete code
    Final product of code

---
+++ CSE311_HW02/src/CalcTest.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalcTest {
+
+	@Test
+	public void positiveSubtractTest() {
+		assertEquals("positiveSubtractTest", Calc.subtract(4, 3), 1);
+	}
+	
+	@Test
+	public void negativeSubtractTest() {
+		assertEquals("negativeSubtractTest", Calc.subtract(3, -4), 7);
+	}
+	
+	@Test
+	public void zeroSubtractTest() {
+		assertEquals("zeroSubtractTest", Calc.subtract(3, 0), 3);
+	}
+
+}
