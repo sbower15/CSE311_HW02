8d721256f44e7c7d5bab191bad643f659a880e7e
Steven Bower <bowersj@miamioh.edu>
2018-03-01 14:59:19 -0500

    Create fault

--- src/Calc.java
+++ src/Calc.java
@@ -14,7 +14,7 @@ public class Calc
 	   return a - b;
    }
    static public int multiply (int a, int b){
-	   return a*b;
+	   return a^b;
    }
    static public int divide (int a, int b){
 	   return a/b;
